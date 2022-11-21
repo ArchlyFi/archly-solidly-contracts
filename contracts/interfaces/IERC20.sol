39f552f3219199cfbf475c7f8aee6565b799d259
tec05 <tec05isalive@outlook.com>
2022-11-20 20:58:43 -0800

    Update IERC20 interface

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -2,11 +2,15 @@
 pragma solidity 0.8.11;
 
 interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
     function totalSupply() external view returns (uint256);
-    function transfer(address recipient, uint amount) external returns (bool);
-    function decimals() external view returns (uint8);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function name() external view returns (string memory);
     function symbol() external view returns (string memory);
-    function balanceOf(address) external view returns (uint);
-    function transferFrom(address sender, address recipient, uint amount) external returns (bool);
-    function approve(address spender, uint value) external returns (bool);
+    function decimals() external view returns (uint8);
 }
